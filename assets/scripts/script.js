6950aef98238f21406018a7a5411e5bcff4f5d1c
tomato23d <anna.a.art@gmail.com>
2023-03-29 16:32:12 +1100

    data load is tested fine

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,33 +1,46 @@
-var btnSearch = document.getElementById("search-api");
+var btnSearch = document.getElementById("search-apig");
+var btnByCitySearch = document.getElementById("search-apicity");
+
 var outputText = document.getElementById("h5par");
 
-var myKey = "bf4cae612aea9c8fdcead6bf50e7112e";
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=bf4cae612aea9c8fdcead6bf50e7112e';
+
+//var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=' + myKey;
 var city = "Paris";
+var myKey = "612789a87d35fc20d850942f4f954d5d";
 
-function testButton(){console.log("hello")};
+var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=48.8534&lon=2.3488&appid=bf4cae612aea9c8fdcead6bf50e7112e';
+var weatherCityUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Paris&appid=bf4cae612aea9c8fdcead6bf50e7112e';
 
-
-function getApi(){
+function getApi(event){
+   event.preventDefault();
     weatherUrl
     
-    fetch(weatherUrl)
+   fetch(weatherUrl)
     .then(function(response){
-       return response.json();
-        })
+      return response.json();
+         })
     .then(function(data){
-       console.log(data)
-        })
-
+        console.log("weather", data)
         for (var i = 0; i < data.length; i++) {
-    
-    h5par.textContent = data[i].name;
+                h5par.textContent = data[i].name;
+        }
+        })
+     };
 
 
-    }};
 
+     function getApiCity(event){
+        event.preventDefault();
+         weatherCityUrl;
+         
+        fetch(weatherUrl)
+         .then(function(response){
+           return response.json();
+              })
+         .then(function(Citydata){
+             console.log("weatherCity", Citydata)})};
 
+btnSearch.addEventListener('click', getApi);
+btnByCitySearch.addEventListener('click', getApiCity);
 
-//btnSearch.addEventListener('click', getApi);
-
-btnSearch.addEventListener('click', testButton);
+//btnSearch.addEventListener('click', testButton);
